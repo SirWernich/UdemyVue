bca5df109f73c9a3538c443e8c001ee7aedd65c6
Wernich Wilsenach <wernich@chasesoftware.co.za>
2019-02-10 10:40:08 +0200

    Added axios examples

---
+++ 22_axios/src/components/dashboard/dashboard.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="dashboard">
+        <h1>That's the dashboard!</h1>
+        <p>You should only get here if you're authenticated!</p>
+        <p>your email address: {{ email }}</p>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      email: ''
+    };
+  },
+    created() {
+        axios
+            .get('users.json')
+            .then(res => {
+                console.log(res.data);
+                const users = [];
+
+                for ( const key in res.data) {
+                  const user = res.data[key];
+                  user.id = key;
+
+                  users.push(user);
+                }
+                console.log(users)
+
+                this.email = users[0].email;
+            })
+            .catch(console.log);
+    }
+};
+</script>
+
+<style scoped>
+h1,
+p {
+    text-align: center;
+}
+
+p {
+    color: red;
+}
+</style>
